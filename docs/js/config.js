177653bb5e38e2179a57f52b94f1f2d90ab15846
Volibs <Volibearinter@gmail.com>
2026-09-25 20:54:17 +0300

    Delete docs directory

--- docs/js/config.js
+++
@@ -1,5 +0,0 @@
-/* Filled in once the online leaderboard (Firebase Realtime Database) is set up. See README. */
-window.NEXUS_CONFIG = {
-  leaderboardDb: "",        // e.g. "https://nexushub-lb-default-rtdb.firebaseio.com"
-  repo: "Volibs/nexushub"
-};
