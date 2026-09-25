cf3dda3ab2b038273b3f5f27ffd2db3a28d5e0b0
Volibs <Volibearinter@gmail.com>
2026-09-25 22:18:32 +0300

    Add files via upload

---
+++ docs/js/config.js
@@ -0,0 +1,5 @@
+/* Filled in once the online leaderboard (Firebase Realtime Database) is set up. See README. */
+window.COMPUBASE_CONFIG = {
+  leaderboardDb: "",        // e.g. "https://compubase-lb-default-rtdb.firebaseio.com"
+  repo: "Volibs/Compubase"
+};
